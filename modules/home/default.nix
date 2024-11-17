737d8f1cf2ba8fda88367315a1ae4d5a7a807bfc
Jason Ernst <ernstjason1@gmail.com>
2024-11-17 12:35:42 -0800

    WiP

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -3,4 +3,10 @@
   imports = [
     ./git.nix
   ];
+
+  home.packages = with pkgs; [
+    pkgs._1password
+  ];
+
+  home.stateVersion = "24.05";	
 }
--- modules/home/gui.nix
+++ modules/home/gui.nix
@@ -3,4 +3,11 @@
   imports = [
     ./1password.nix
   ];
+
+  home.packages = with pkgs; [
+    pkgs._1password-gui
+  ];
+
+  home.stateVersion = "24.05";
+
 }
