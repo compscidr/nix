1a5f1506d24e17c08673133abc5e4baaa12efcb8
jason <ernstjason1@gmail.com>
2024-11-18 08:43:44 -0800

    Added speedtest-cli

--- common/network-tools.nix
+++ common/network-tools.nix
@@ -1,19 +1,20 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    arp-scan  # for ip <-> mac map search
-    dnsutils  # dig
-    ethtool   # gets info like link speed
-    iftop     # network top
-    nettools  # ifconfig, netstat, arp
-    nmap      # determine open ports
-    nbtscan   # netbios scanner
-    sshuttle  # forward ssh through tunnel
-    whois     # whois
-    socat     # sort of like nc but doesn't hang
-    sshfs     # ssh mounts
-    iptables  # packet filtering rules
-    fail2ban  # bans repeated attempts at logins from IPs
-    # tcpstat # doesn't exist on nix
+    arp-scan      # for ip <-> mac map search
+    dnsutils      # dig
+    ethtool       # gets info like link speed
+    iftop         # network top
+    nettools      # ifconfig, netstat, arp
+    nmap          # determine open ports
+    nbtscan       # netbios scanner
+    sshuttle      # forward ssh through tunnel
+    whois         # whois
+    socat         # sort of like nc but doesn't hang
+    sshfs         # ssh mounts
+    iptables      # packet filtering rules
+    fail2ban      # bans repeated attempts at logins from IPs
+    speedtest-cli # cli interface for testing bandwidth
+    # tcpstat     # doesn't exist on nix
   ];
 }
