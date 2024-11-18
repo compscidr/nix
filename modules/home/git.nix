f9250dc2e1a48d6f92b4387715e0dd333848c9cc
jason <ernstjason1@gmail.com>
2024-11-17 18:24:24 -0800

    Added 1password ssh gpg signing

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,5 +3,20 @@
     enable = true;
     userName = "jason";
     userEmail = "ernstjason1@gmail.com";
+
+    extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "op-ssh-sign";
+      };
+      commit = {
+        gpgsign = true;
+      };
+      user = {
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGDMaQs+szAMlBHJk07vZf7HUUjOUExOQCeVLd7Lg+mD";
+      };
+    };
   };
 }
