c544f31c9733c4a3c35fdac19d4fbb2ed923a60d
jason <ernstjason1@gmail.com>
2024-11-18 08:48:05 -0800

    removed sqlite3, doesn't exist

--- common/cli-tools.nix
+++ common/cli-tools.nix
@@ -4,9 +4,9 @@
     btop         # resource monitor
     # btop++     # doesn't exist on nix
     htop         # interactive process viewer
-    # lm-sensors # doesn't exist on nixinsights into cpu temp, other sensors
+    # lm-sensors # doesn't exist on nix. insights into cpu temp, other sensors
     wget         # alternative to curl
-    sqlite3      # useful for dev work
+    # sqlite3    # doesn't exist on nix. useful for dev work
     cheat        # interactive cheatsheets on the cli
   ];
 }
