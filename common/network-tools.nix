d7411dab5e0c06be57c00a7e196b7bffed7f13a6
jason <ernstjason1@gmail.com>
2024-11-18 08:36:57 -0800

    Added various network tools

--- common/network-tools.nix
+++ common/network-tools.nix
@@ -1,6 +1,19 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    arp-scan
-  ]
+    arp-scan  # for ip <-> mac map search
+    dnsutils  # dig
+    ethtool   # gets info like link speed
+    iftop     # network top
+    nettools  # ifconfig, netstat, arp
+    nmap      # determine open ports
+    nbtscan   # netbios scanner
+    sshuttle  # forward ssh through tunnel
+    whois     # whois
+    socat     # sort of like nc but doesn't hang
+    sshfs     # ssh mounts
+    iptables  # packet filtering rules
+    fail2ban  # bans repeated attempts at logins from IPs
+    # tcpstat # doesn't exist on nix
+  ];
 }
