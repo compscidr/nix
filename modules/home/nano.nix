c2527503e761a038a09b274827178fe86797e616
jason <ernstjason1@gmail.com>
2024-11-18 09:22:12 -0800

    WiP

--- modules/home/nano.nix
+++ modules/home/nano.nix
@@ -1,5 +1,15 @@
+{ pkgs, ... }:
 {
-  programs.nano = {
+  home.packages = with pkgs; [
+    nano
+  ];
 
-  }
+  programs.nano = {
+    enable = true;
+    nanorc = "''
+      set linenumbers true
+      set guidestripe 80
+    ''";
+    syntaxHighlight = true;
+  };
 }
