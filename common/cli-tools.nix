b303718dbd1640bc31010c39fa721348cb39826c
jason <ernstjason1@gmail.com>
2024-11-18 08:45:47 -0800

    commented out lm-sensors, doesn't exist. Added cheat

--- common/cli-tools.nix
+++ common/cli-tools.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    btop       # resource monitor
-    # btop++   # doesn't exist on nix
-    htop       # interactive process viewer
-    lm-sensors # insights into cpu temp, other sensors
-    wget       # alternative to curl
-    sqlite3    # useful for dev work
+    btop         # resource monitor
+    # btop++     # doesn't exist on nix
+    htop         # interactive process viewer
+    # lm-sensors # doesn't exist on nixinsights into cpu temp, other sensors
+    wget         # alternative to curl
+    sqlite3      # useful for dev work
+    cheat        # interactive cheatsheets on the cli
   ];
 }
