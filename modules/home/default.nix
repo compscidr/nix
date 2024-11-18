456d4c63751321b3f20a88a14c0e7e1281671696
jason <ernstjason1@gmail.com>
2024-11-17 17:38:07 -0800

    updated so that we use the 1password ssh agent

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -7,4 +7,12 @@
   home.packages = with pkgs; [
     pkgs._1password
   ];
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+          IdentityAgent "~/.1password/agent.sock"
+    '';
+  };
 }
