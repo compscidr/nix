ffb3782e9d4a37103b356e572449391d6d9dae9a
jason <ernstjason1@gmail.com>
2024-11-18 21:50:32 -0800

    disabled auto suspend

--- machines/cube/configuration.nix
+++ machines/cube/configuration.nix
@@ -50,13 +50,7 @@
   # services.xserver.displayManager.lightdm.enable = true; 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-  # If no user is logged in, the machine will power down after 20 minutes.
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
+  services.xserver.displayManager.gdm.autoSuspend = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
